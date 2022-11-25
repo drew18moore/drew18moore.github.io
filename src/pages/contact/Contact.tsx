8f1ed849905ff90baf2cc80e2eb689d3bcd131fc
Drew <dcm752052@gmail.com>
2022-11-25 00:37:04 -0500

    Improved styling of contact form.

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,17 +1,40 @@
-import "./contact.css"
+import "./contact.css";
 
 function Contact() {
   return (
     <main id="contact-main">
-      <h1>Contact</h1>
-      <form id="contact-form" action="https://formsubmit.co/drewmoore052@gmail.com" method="POST">
-        <input type="text" name="name" id="name" placeholder="Name" required />
-        <input type="email" name="email" id="email" placeholder="Email" required />
-        <textarea name="message" id="message" placeholder="Message" required></textarea>
-        <button type="submit">Send Message</button>
-      </form>
+      <div id="contact-form-container">
+        <h1>Contact</h1>
+        <form
+          id="contact-form"
+          action="https://formsubmit.co/drewmoore052@gmail.com"
+          method="POST"
+        >
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            required
+          ></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </main>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
