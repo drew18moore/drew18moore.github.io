01ff80952701d132de066b242a21d409932d1a49
Drew <dcm752052@gmail.com>
2022-11-24 23:36:43 -0500

    Hamburger menu now closes when user clicks link.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,23 +8,23 @@ function Navbar() {
     <header>
       <nav>
         <div id="nav-logo">
-          <Link to="/">Drew Moore</Link>
+          <Link onClick={() => setIsActive(false)} to="/">Drew Moore</Link>
         </div>
         <ul id="nav-links" className={isActive ? "active" : ""}>
           <li>
-            <Link to="/">Home</Link>
+            <Link onClick={() => setIsActive(false)} to="/">Home</Link>
           </li>
           <li>
-            <Link to="/projects">Projects</Link>
+            <Link onClick={() => setIsActive(false)} to="/projects">Projects</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link onClick={() => setIsActive(false)} to="/contact">Contact</Link>
           </li>
           <li id="nav-socials">
-            <a href="https://www.linkedin.com/in/drew18moore/" className="nav-social-link" target={"_blank"}>
+            <a  onClick={() => setIsActive(false)} href="https://www.linkedin.com/in/drew18moore/" className="nav-social-link" target={"_blank"}>
               <img src="logos/linkedin-icon.png" alt="LinkedIn Logo" />
             </a>
-            <a href="https://github.com/drew18moore" className="nav-social-link" target={"_blank"}>
+            <a  onClick={() => setIsActive(false)} href="https://github.com/drew18moore" className="nav-social-link" target={"_blank"}>
               <img src="logos/github-icon.png" alt="GitHub Logo" />
             </a>
           </li>
