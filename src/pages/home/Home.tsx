6f2c4e4a255ddb1e6d78c11e79f1ba6e90720ac3
Drew <dcm752052@gmail.com>
2023-01-19 00:01:07 -0500

    Replaced resume button with an anchor linked to google docs resume document.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,15 +3,17 @@ import Skill from "../../components/skill/Skill";
 import "./home.css";
 
 function Home() {
-  const [skills, setSkills] = useState<{img: String, language: String}[]>([])
+  const [skills, setSkills] = useState<{ img: String; language: String }[]>([]);
 
   useEffect(() => {
-    fetch('data/skills.json').then((res) => {
-      return res.json()
-    }).then((data) => {
-      setSkills(data)
-    })
-  }, [])
+    fetch("data/skills.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setSkills(data);
+      });
+  }, []);
 
   return (
     <main id="home-main">
@@ -19,19 +21,23 @@ function Home() {
         <img src="profile-picture.png" alt="Profile Picture" />
         <div id="profile-text">
           <h1>Hello There!</h1>
-          <h2>
-            I am a front-end developer with a degree in Computer Science
-          </h2>
-          <form id="resume-form" method="get" action="resume.pdf" target="_blank">
-            <button id="resume-btn" type="submit">Resume</button>
-          </form>
+          <h2>I am a front-end developer with a degree in Computer Science</h2>
+          <a
+            id="resume-link"
+            href="https://docs.google.com/document/d/1ctwFuZzO8t8hQVjV7E8AkNGchEV_LvuLStRwJ-TeEY0/edit?usp=sharing"
+            target="_blank"
+          >
+            Resume
+          </a>
         </div>
       </div>
       <div id="skills-card">
         <h1>Skills</h1>
         <ul id="skills-list">
           {skills.map((skill, index) => {
-            return <Skill img={skill.img} language={skill.language} key={index}/>
+            return (
+              <Skill img={skill.img} language={skill.language} key={index} />
+            );
           })}
         </ul>
       </div>
